72251c832a7efa56e4f95c88c832d9740e08a241
Elena Bravo <hey@elenabravo.com>
2020-10-25 13:36:46 +0100

    add render props WithAddToCartProps component

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -19,3 +19,15 @@ export function withAddToCart<OriginalProps extends AddToCartProps>(ChildCompone
     }
     return AddToCartHOC
 }
+
+export const WithAddToCartProps: React.FC<{children: (props: AddToCartProps) => JSX.Element}> = ({ children }) => {
+    const dispatch = useStateDispatch()
+    const addToCart: AddToCartProps['addToCart'] = (item) => {
+        dispatch({
+            type: 'ADD_TO_CART',
+            payload: { item }    
+        })
+    }
+
+    return children({ addToCart })
+}
--- src/components/SpecialOffer.tsx
+++ src/components/SpecialOffer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Pizza } from '../types'
 import SpecialOfferCSS from './SpecialOffer.module.css'
-import { withAddToCart, AddToCartProps } from './AddToCart'
+import { WithAddToCartProps, AddToCartProps } from './AddToCart'
 
 interface Props extends AddToCartProps{
     pizza: Pizza
@@ -9,20 +9,21 @@ interface Props extends AddToCartProps{
 
 const SpecialOffer: React.FC<Props> = ({ pizza, addToCart }) => {
     const { name, description, price } = pizza
-    const handleAddToCart = () => {
-        addToCart({ ...pizza })
-    }
 
     return (
         <div className={SpecialOfferCSS.container}>
             <h2>{name}</h2>
             <p>{description}</p>
             <p>${price}</p>
-            <button type="button" onClick={handleAddToCart}>
-                Add to cart
-            </button>
+            <WithAddToCartProps>
+                {({ addToCart }) => (
+                    <button type="button" onClick={() => addToCart({...pizza})}>
+                        Add to cart
+                    </button>
+                )}
+            </WithAddToCartProps>
         </div>
     )
 }
 
-export default withAddToCart(SpecialOffer)
+export default SpecialOffer
